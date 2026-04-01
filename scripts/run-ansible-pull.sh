791ec11fe420e625a39e8ff361a0875e02fb02cd
Henry Ngo <ngoh10@janelia.hhmi.org>
2026-04-01 09:28:12 -0400

    Avoid ansible-pull git lock races

--- scripts/run-ansible-pull.sh
+++ scripts/run-ansible-pull.sh
@@ -18,6 +18,7 @@ HOSTNAME_SHORT="$(hostname -s)"
 HOSTNAME_FQDN="$(hostname -f 2>/dev/null || hostname)"
 RUN_LOG="${LOG_DIR}/ansible-pull-${HOSTNAME_SHORT}.log"
 RUNTIME_INVENTORY="/etc/ansible/pull-inventory.yml"
+LOCK_FILE="/var/lock/ansible-pull.lock"
 
 cat > "${RUNTIME_INVENTORY}" <<EOF
 all:
@@ -33,13 +34,33 @@ all:
       ansible_python_interpreter: /usr/bin/python3
 EOF
 
-exec /usr/bin/ansible-pull \
-  --url "${REPO_URL}" \
-  --checkout "${BRANCH}" \
-  --directory "${DEST}" \
-  --inventory "${RUNTIME_INVENTORY}" \
-  --limit localhost \
-  --accept-host-key \
-  --clean \
-  -e ansible_python_interpreter=/usr/bin/python3 \
-  "${PLAYBOOK}" >> "${RUN_LOG}" 2>&1
+exec 9>"${LOCK_FILE}"
+
+if ! flock -n 9; then
+  {
+    echo "Another ansible-pull run is already in progress. Exiting."
+  } >> "${RUN_LOG}"
+  exit 0
+fi
+
+{
+  echo "Starting Ansible Pull at $(date '+%Y-%m-%d %H:%M:%S')"
+
+  if [[ -d "${DEST}/.git" ]]; then
+    rm -f "${DEST}/.git/index.lock" "${DEST}/.git/shallow.lock" "${DEST}/.git/HEAD.lock"
+    git -C "${DEST}" fetch origin "${BRANCH}"
+    git -C "${DEST}" checkout "${BRANCH}"
+    git -C "${DEST}" reset --hard "origin/${BRANCH}"
+  else
+    rm -rf "${DEST}"
+    git clone --branch "${BRANCH}" "${REPO_URL}" "${DEST}"
+  fi
+
+  cd "${DEST}"
+
+  /usr/bin/ansible-playbook \
+    --inventory "${RUNTIME_INVENTORY}" \
+    --limit localhost \
+    -e ansible_python_interpreter=/usr/bin/python3 \
+    "${PLAYBOOK}"
+} >> "${RUN_LOG}" 2>&1
